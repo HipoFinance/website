78419c253cc68a29dc70665fdc6b23570ce90669
Behrang Norouzinia <behrangn@gmail.com>
2023-07-02 13:56:11 +0330

    Remove old website

--- build-pages.sh
+++
@@ -1,2 +0,0 @@
-npm run build && mv dist ../ && git switch pages && rm -rf assets fonts icons index.html manifest.json public favicon.ico website app.html && mv ../dist/* . && rm -r ../dist && cp index.html app.html && git add -A && git commit -m 'update' && git push hipo pages && git switch main
-
