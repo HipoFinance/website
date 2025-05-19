3c1b782ea79f093691381419be8663bcbebfb8a4
Alireza Ravanbakhsh <alireza.ravanbakhsh@gmail.com>
2025-05-19 16:51:34 +0330

    New design - first version

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "@fontsource/heebo/latin-400.css";
+import "@fontsource/heebo/latin-500.css";
+import "@fontsource/heebo/latin-700.css";
+import "@fontsource/eczar/latin-700.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
