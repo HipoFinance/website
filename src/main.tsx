eecf86cddc45c4538e9af0887236423ab053c962
Alireza Ravanbakhsh <alireza.ravanbakhsh@gmail.com>
2025-05-20 16:59:51 +0330

    Update title and description

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./styles.css";
-import "@fontsource/heebo/latin-400.css";
-import "@fontsource/heebo/latin-500.css";
-import "@fontsource/heebo/latin-700.css";
-import "@fontsource/eczar/latin-700.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './styles.css'
+import '@fontsource/heebo/latin-400.css'
+import '@fontsource/heebo/latin-500.css'
+import '@fontsource/heebo/latin-700.css'
+import '@fontsource/eczar/latin-700.css'
+import '@fontsource/poppins/latin-400.css'
+import '@fontsource/poppins/latin-500.css'
+import '@fontsource/poppins/latin-700.css'
+import '@fontsource/poppins/latin-900.css'
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+)
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,53 +1,55 @@
-import type { Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
+import type { Config } from 'tailwindcss'
+import { fontFamily } from 'tailwindcss/defaultTheme'
 
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    fontFamily: {
-      heebo: ["Heebo", ...fontFamily.sans],
-      eczar: ["Eczar"],
-    },
-    extend: {
-      colors: {
-        black: "#03010d",
-        white: "#ffffff",
-        purple1: "#6d1eb3",
-        purple2: "#461472",
-        purple3: "#dfbcfe",
-        purple4: "#d8d8f6",
-        purple5: "#cc60fe",
-        purple6: "#7e22ce",
-        purple7: "#471fb7",
-        purple8: "#8753ff",
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        fontFamily: {
+            heebo: ['Heebo', ...fontFamily.sans],
+            eczar: ['Eczar'],
+            poppins: ['Poppins'],
+        },
+        extend: {
+            colors: {
+                black: '#03010d',
+                white: '#ffffff',
+                purple1: '#6d1eb3',
+                purple2: '#461472',
+                purple3: '#dfbcfe',
+                purple4: '#d8d8f6',
+                purple5: '#4411d8',
+                purple6: '#7e22ce',
+                purple7: '#471fb7',
+                purple8: '#8753ff',
+                purple9: '#cc60fe',
 
-        pink1: "#e622c2",
-        darkblue1: "#170c55",
-        darkblue2: "#03010d",
-        darkblue3: "#0B0826",
-        darkblue4: "#1d1356",
-        darkblue5: "#3e337d",
-        gray1: "#dedbe4",
-        background: "#02031e",
-      },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        brush: "url(/brush.svg)",
-      },
-      rotate: {
-        "30": "30deg",
-        "-30": "-30deg",
-      },
-      width: {
-        "1/7": "14.28571429%",
-        "2/7": "28.57142857%",
-        "3/7": "42.85714286%",
-        "4/7": "57.14285714%",
-        "5/7": "71.42857143%",
-        "6/7": "85.71428571%",
-        "7/7": "100%",
-      },
+                pink1: '#e622c2',
+                darkblue1: '#170c55',
+                darkblue2: '#03010d',
+                darkblue3: '#0B0826',
+                darkblue4: '#1d1356',
+                darkblue5: '#3e337d',
+                gray1: '#dedbe4',
+                background: '#02031e',
+            },
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                brush: 'url(/brush.svg)',
+            },
+            rotate: {
+                '30': '30deg',
+                '-30': '-30deg',
+            },
+            width: {
+                '1/7': '14.28571429%',
+                '2/7': '28.57142857%',
+                '3/7': '42.85714286%',
+                '4/7': '57.14285714%',
+                '5/7': '71.42857143%',
+                '6/7': '85.71428571%',
+                '7/7': '100%',
+            },
+        },
     },
-  },
-  plugins: [],
-} satisfies Config;
+    plugins: [],
+} satisfies Config
