f91c1ddf21e73ce05071033c98d60ead853e63e5
Alireza Ravanbakhsh <alireza.ravanbakhsh@gmail.com>
2026-06-28 12:58:36 +0330

    Use @astrojs/sitemap library to generate sitemap.xml automatically on build.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@tailwindcss/vite'
 import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
   site: 'https://hipo.finance',
@@ -11,5 +12,5 @@ export default defineConfig({
     plugins: [tailwind()],
   },
 
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 })
